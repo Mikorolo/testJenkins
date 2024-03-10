96b09c672342badf656a92ad3cfc21d9043b7342
Mikorolo <76916634+Mikorolo@users.noreply.github.com>
2024-03-11 00:27:26 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,12 +1,21 @@
 import unittest
 from app import dodaj
 
-class TestDodaj(unittest.TestCase):
-
-    def test_dodaj(self):
+class TestArytmetyki(unittest.TestCase):
+    def test_dodawanie(self):
         self.assertEqual(dodaj(3, 5), 8)
-        self.assertEqual(dodaj(-1, 1), 0)
-        self.assertEqual(dodaj(0, 0), 0)
+
+    def test_odejmowanie(self):
+        self.assertEqual(odejmij(10, 4), 6)
+
+    def test_mnozenie(self):
+        self.assertEqual(pomnoz(2, 3), 6)
+
+    def test_dzielenie(self):
+        self.assertEqual(podziel(10, 2), 5)
+        self.assertEqual(podziel(10, 5), 2)
+        with self.assertRaises(ValueError):
+            podziel(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
